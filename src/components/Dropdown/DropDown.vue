989a99427aa21e7c321fa4f563468e01c665b55f
Lakshya Khera <lakshya.khera@gmail.com>
2020-05-01 05:41:21 +0530

    Dropdown added

--- src/components/Dropdown/DropDown.vue
+++ src/components/Dropdown/DropDown.vue
@@ -1,9 +1,10 @@
 <template>
-    <div>
+    <div class="input-wrapper">
         <select>
-
+            <option>Hi</option>
+            <option>World</option>
         </select>
-    <div>
+    </div>
 </template>
 
 <script>
@@ -11,3 +12,35 @@ export default {
     name:'DropDown'
 }
 </script>
+
+<style scoped>
+.input-wrapper {
+    padding:10px 15px;
+    min-width: 200px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border:1px solid rgba(0, 0, 0, 0.25);
+    border-radius: 10px;
+    box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.25);
+}
+select {
+    font-size:18px;
+    background:transparent;
+    appearance:none;
+	-webkit-appearance:none;
+	-moz-appearance:none;
+	-ms-appearance:none;
+	border:2px solid #000;
+	background:url('../../assets/drop-icon.png');
+	background-repeat:no-repeat;
+	background-size:16px 17px;
+    background-position:right 2px;
+    flex-grow: 2;
+    border:0;
+
+}
+select:focus {
+    outline: none;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Dropdown from './components/Dropdown/Dropdown.vue'
+import ToggleEye from  './components/EyeWrapper/ToggleEye.component.vue'
+import PasswordInput from './components/PasswordInput/PasswordInput.vue'
+import TextInput from './components/TextInput/TextInput.vue'
+
+export {
+    Dropdown,
+    ToggleEye,
+    PasswordInput,
+    TextInput
+}
